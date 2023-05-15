1c21d12fcbdcb572165a6f14fd218e47fbf4e3c0
Matthias Fisch <mail@matthias-fisch.de>
2023-05-15 19:53:56 +0200

    Add endpoint to get application information

--- src/main/kotlin/de/matthiasfisch/planningpoker/model/APIModel.kt
+++ src/main/kotlin/de/matthiasfisch/planningpoker/model/APIModel.kt
@@ -29,3 +29,14 @@ data class PagedResult<T>(
         page.totalPages
     )
 }
+
+// Info
+
+data class ApplicationInfo(
+    val socketIO: SocketIOInfo
+)
+
+data class SocketIOInfo(
+    val host: String,
+    val port: Int
+)
